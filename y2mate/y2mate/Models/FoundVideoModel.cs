fcf25b38695b3413e156d3581050b79abe78eb61
RxiPland <liborvanura@gmail.com>
2023-08-14 02:18:00 +0200

    Created model for found videos

---
+++ y2mate/y2mate/Models/FoundVideoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace y2mate.Models
+{
+    public class FoundVideoModel
+    {
+
+        // {"360p": "oFGWtSzxsagasL176v...", }
+        public Dictionary<string, string> AvailableMP3 = new();
+        public Dictionary<string, string> AvailableMP4 = new();
+
+        public string VideoTitle { get; set; }
+        public int VideoDurationTimeSec { get; set; }
+        public string VideoId { get; set; }
+        public string YtChannel { get; set; }
+
+    }
+}
